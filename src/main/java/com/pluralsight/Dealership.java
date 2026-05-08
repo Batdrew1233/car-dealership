454682b379ec21f926d50b55229566b7cc7c93a6
Batdrew1233 <drewstevenson1233@gmail.com>
2026-05-08 14:12:49 -0400

    Completed outline for Dealership Class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,69 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        inventory = new ArrayList<>();
+    }
+
+    public Vehicle getVehiclesByPrice(double min, double max){
+        return null;
+    }
+
+    public Vehicle getVehiclesByPrice(String make, String model){
+        return null;
+    }
+
+    public Vehicle getVehiclesByYear(double min, double max){
+        return null;
+    }
+
+    public Vehicle getVehiclesByColor(String color){
+        return null;
+    }
+
+    public Vehicle getVehiclesByMileage(double min, double max){
+        return null;
+    }
+
+    public Vehicle getVehicleByType(String vehicleType){
+        return null;
+    }
+
+    public Vehicle getAllVehicles(){
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,4 @@
 package com.pluralsight;
 
-public class Vehichle {
+public class Vehicle {
 }
